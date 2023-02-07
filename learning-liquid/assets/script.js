d4aff3ba13eadf74dff42cd16bef6a06dfcdb37d
Shubham Shinde <shubhamshinde281999@gmail.com>
2023-02-07 09:51:16 +0530

    sorting and filteration work but the serch.liquid section is not working

--- learning-liquid/assets/script.js
+++ learning-liquid/assets/script.js
@@ -2,15 +2,15 @@
 
 // // Get the current URL query parameters and store them in an object
 const addExistingQuery = function(queryParams,queryArray){
-if (queryParams.length) {               //location.search  return "?key1=value1&key2=value2"
-  console.log("filter to sort", location.search);
-  const keyValuePairs = queryParams.split("&");  //["key=value", "key-value", ...]
-  for (const keyValue of keyValuePairs) {
-    const [key, value] = keyValue.split("=");   //key   value
-    if(decodeURIComponent(key) !== "sort_by")
-      queryArray.push(`${decodeURIComponent(key)}=${decodeURIComponent(value)}`);//https%3A%2F%2Fwww.flowjoe.io%2Fexample%20page //: = %3A, / = %2F, space = %20 //make it human readable //https://www.flowjoe.io/example page                                                                  
-  }                                                                      
-}
+  if (queryParams.length) {               //location.search  return "?key1=value1&key2=value2"
+    console.log("filter to sort", location.search);
+    const keyValuePairs = queryParams.split("&");  //["key=value", "key-value", ...]
+    for (const keyValue of keyValuePairs) {
+      const [key, value] = keyValue.split("=");   //key   value
+      if(decodeURIComponent(key) !== "sort_by")
+        queryArray.push(`${decodeURIComponent(key)}=${decodeURIComponent(value)}`);//https%3A%2F%2Fwww.flowjoe.io%2Fexample%20page //: = %3A, / = %2F, space = %20 //make it human readable //https://www.flowjoe.io/example page                                                                  
+    }                                                                      
+  }
 }
 
 // When the user selects a different sorting option, update the query parameters and reload the page
